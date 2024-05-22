f01804fc43dd09b93ad0f23cd1260463d75ab0c1
takaoka sayuko <sp7y3dx9snn@gmail.com>
2024-05-22 23:55:28 +0900

    打刻機能の追加

--- src/app/Http/Controllers/AttendanceController.php
+++ src/app/Http/Controllers/AttendanceController.php
@@ -14,29 +14,30 @@ class AttendanceController extends Controller
   public function index()
   {
     $user = Auth::user();
-    
+
     //ボタンの活性・非活性
     $status_work = Attendance::where('user_id', $user->id)
-      ->latest('updated_at')
+      ->latest('id')
       ->first();
+    $attendance_id = Attendance::where('user_id', $user->id)
+      ->latest('id')
+      ->first('id');
 
-    $attendance_id =
-        Attendance::where('user_id', $user->id)
-        ->latest('updated_at')
-        ->first('id');
-
-    if(empty($attendance_id) == true || empty($status_work['work_end']) == false){
-      $button = 0;//出勤ボタンを活性
-    } else{
+    if (!empty($attendance_id)) {
       $status_rest =
         Rest::where('attendance_id', $attendance_id->id)
-        ->latest('updated_at')
+        ->latest('id')
         ->first();
-      if (empty($status_rest['rest_end']) == false && empty($status_work['work_end']) == true) {
-      $button = 1;//休憩開始と退勤ボタンを活性
-      } else {
-        $button = 2;//休憩終了を活性
-      };
+    };
+
+
+
+    if (empty($attendance_id) == true || empty($status_work['work_end']) == false) {
+      $button = 0; //出勤ボタンを活性
+    } elseif (empty($status_rest['rest_start']) == false && empty($status_rest['rest_end']) == true) {
+      $button = 2; //休憩終了ボタンを活性
+    } else {
+      $button = 1; //休憩開始と退勤ボタンを活性
     };
 
     return view('index', compact('user', 'button'));
@@ -47,6 +48,7 @@ class AttendanceController extends Controller
   {
     $user_id = Auth::id();
     $work_start = new Carbon();
+
     Attendance::create([
       'user_id' => $user_id,
       'work_start' => $work_start
@@ -58,14 +60,47 @@ class AttendanceController extends Controller
   public function update()
   {
     $user = Auth::user();
-    $work_end = new Carbon;
-    Attendance::where('user_id', $user->id)
-    ->latest('updated_at')
-    ->first()
-    ->update([
-      'work_end' => $work_end
-    ]);
-    return redirect('/');
 
-}
+    $today = Carbon::now();
+    $today_date = $today->toDateString();
+    $work_start =
+      Attendance::where('user_id', $user->id)
+      ->latest('id')
+      ->first();
+    $start_date = Carbon::parse($work_start['work_start']);
+    $date = $start_date->toDateString();
+
+    //出勤日と退勤日が異なる場合
+    while ($date < $today_date) {
+      //退勤時間を登録
+      $end_time = '23:59:59';
+      $end_date_string = $date . ' ' . $end_time;
+      $end_date = Carbon::createFromFormat('Y-m-d H:i:s', $end_date_string);
+      Attendance::where('user_id', $user->id)
+        ->latest('id')
+        ->first()
+        ->update([
+          'work_end' => $end_date
+        ]);
+
+      //翌日に出勤時間を登録
+      $start_time = '00:00:00';
+      $date = $start_date->addDay()->toDateString();
+      $next_date_string = $date . ' ' . $start_time;
+      $work_start = Carbon::createFromFormat('Y-m-d H:i:s', $next_date_string);
+      Attendance::create([
+        'user_id' => $user->id,
+        'work_start' => $work_start
+      ]);
+    };
+
+    Attendance::where('user_id', $user->id)
+      ->latest('id')
+      ->first()
+      ->update([
+        'work_end' => $today
+      ]);
+
+    return redirect('/');
+  }
 }
--- src/app/Http/Controllers/RestController.php
+++ src/app/Http/Controllers/RestController.php
@@ -10,43 +10,129 @@ use Illuminate\Http\Request;
 
 class RestController extends Controller
 {
-    //休憩開始
-    public function store()
-    {
-        $user = Auth::user();
-        $rest_start = new Carbon;
-        $attendance_id =
-        Attendance::where('user_id', $user->id)
-        ->latest('updated_at')
-        ->first('id');
-        Rest::create([
-            'attendance_id' => $attendance_id['id'],
-            'rest_start' => $rest_start
-        ]);
+  //休憩開始
+  public function store()
+  {
+    $user = Auth::user();
 
-        return redirect('/');
+    $today = Carbon::now();
+    $today_date = $today->toDateString();
+    $work_start =
+      Attendance::where('user_id', $user->id)
+      ->latest('id')
+      ->first();
+    $start_date = Carbon::parse($work_start['work_start']);
+    $date = $start_date->toDateString();
 
-    }
-
-    //休憩終了
-    public function update()
-    {
-        $user = Auth::user();
-       
-        $attendance_id =
-        Attendance::where('user_id', $user->id)
-        ->latest('updated_at')
-        ->first('id');
-        $rest_end = new Carbon;
-    
-        Rest::where('attendance_id', $attendance_id->id)
-        ->latest('updated_at')
+    //出勤日と休憩開始日が異なる場合
+    while ($date < $today_date) {
+   
+      //退勤時間を登録
+      $end_time = '23:59:59';
+      $end_date_string = $date . ' ' . $end_time;
+      $end_date = Carbon::createFromFormat('Y-m-d H:i:s', $end_date_string);
+      Attendance::where('user_id', $user->id)
+        ->latest('id')
         ->first()
         ->update([
-            'rest_end' => $rest_end
+          'work_end' => $end_date
         ]);
 
-        return redirect('/');
+      //翌日に出勤時間を登録
+      $start_time = '00:00:00';
+      $date = $start_date->addDay()->toDateString();
+      $next_date_string = $date . ' ' . $start_time;
+      $work_start = Carbon::createFromFormat('Y-m-d H:i:s', $next_date_string);
+      Attendance::create([
+        'user_id' => $user->id,
+        'work_start' => $work_start
+      ]);
 
-    }
+    };
+
+    $attendance_id =
+      Attendance::where('user_id', $user->id)
+      ->latest('id')
+      ->first('id');
+    Rest::create([
+      'attendance_id' => $attendance_id['id'],
+      'rest_start' => $today
+    ]);
+
+    return redirect('/');
+  }
+
+  //休憩終了
+  public function update()
+  {
+    $user = Auth::user();
+    $attendance_id =
+      Attendance::where('user_id', $user->id)
+      ->latest('id')
+      ->first('id');
+
+
+    $today = Carbon::now();
+    $today_date = $today->toDateString();
+    $rest_start =
+      Rest::where('attendance_id', $attendance_id->id)
+      ->latest('id')
+      ->first();
+
+    $start_date = Carbon::parse($rest_start['rest_start']);
+    $date = $start_date->toDateString();
+
+    //休憩の開始日と終了日が異なる場合
+    while ($date < $today_date) {
+      //休憩終了時間と退勤時間を登録
+      $end_time = '23:59:59';
+      $end_date_string = $date . ' ' . $end_time;
+      $end_date = Carbon::createFromFormat('Y-m-d H:i:s', $end_date_string);
+
+
+      Rest::where('attendance_id', $attendance_id->id)
+      ->latest('id')
+      ->first()
+      ->update([
+        'rest_end' => $end_date
+      ]);
+
+      Attendance::where('user_id', $user->id)
+        ->latest('id')
+        ->first()
+        ->update([
+          'work_end' => $end_date
+        ]);
+
+      //翌日に出勤時間と休憩開始時間を登録
+      $start_time = '00:00:00';
+      $date = $start_date->addDay()->toDateString();
+      $next_date_string = $date . ' ' . $start_time;
+      $work_start = Carbon::createFromFormat('Y-m-d H:i:s', $next_date_string);
+      Attendance::create([
+        'user_id' => $user->id,
+        'work_start' => $work_start
+      ]);
+
+      $next_id =
+      Attendance::where('user_id', $user->id)
+      ->latest('id')
+      ->first('id');
+      Rest::create([
+        'attendance_id' => $next_id['id'],
+        'rest_start' => $work_start
+      ]);
+
+    };
+
+    Rest::where('attendance_id', $attendance_id->id)
+      ->latest('updated_at')
+      ->first()
+      ->update([
+        'rest_end' => $today
+      ]);
+
+
+    return redirect('/');
+  }
 }
